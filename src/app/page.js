2683ecad265ebb40a8ffb95d480b605dba14c2a3
philip-meie <mixexfp@gmail.com>
2025-10-16 21:10:39 +0700

    Next.js Todo List Assignment3

--- src/app/page.js
+++ src/app/page.js
@@ -3,76 +3,63 @@ export default function Page() {
     <main
       style={{
         fontFamily: "Arial, sans-serif",
-        margin: "60px auto",
-        width: "600px",
-        textAlign: "left",
+        margin: "40px",
       }}
     >
-      {/* Title */}
       <h1
         style={{
           fontWeight: "bold",
           fontSize: "48px",
-          marginBottom: "20px",
-          textAlign: "center",
+          marginBottom: "12px",
         }}
       >
         Todo List App
       </h1>
 
-      {/* Input Section */}
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginBottom: "24px",
-        }}
-      >
+      <div style={{ marginBottom: "16px" }}>
         <input
           type="text"
           placeholder="Enter a new task.."
           style={{
-            padding: "10px",
+            padding: "8px",
             width: "320px",
+            marginRight: "6px",
             border: "1px solid #999",
             borderRadius: "4px",
-            fontSize: "16px",
           }}
         />
         <button
           style={{
-            padding: "10px 16px",
-            marginLeft: "8px",
+            padding: "8px 12px",
             border: "1px solid #999",
             borderRadius: "6px",
-            backgroundColor: "#f3f3f3",
-            cursor: "pointer",
+            backgroundColor: "white",
+            color: "black",
+            fontWeight: "bold",
           }}
         >
           Add
         </button>
       </div>
 
-      {/* Todo List */}
       <ul
         style={{
           listStyleType: "disc",
-          paddingLeft: "40px",
-          fontSize: "22px",
-          lineHeight: "1.8",
+          paddingLeft: "24px",
+          fontSize: "24px",
         }}
       >
-        <li>
+        <li style={{ marginBottom: "8px" }}>
           <input type="checkbox" /> <span>Sleep</span>
         </li>
-        <li>
+        <li style={{ marginBottom: "8px" }}>
           <input type="checkbox" defaultChecked />{" "}
           <span style={{ textDecoration: "line-through", color: "gray" }}>
             Join React class
           </span>
         </li>
-        <li>
-          <input type="checkbox" /> <span>Do React homework</span>
+        <li style={{ marginBottom: "8px" }}>
+          <input type="checkbox" /> <span>Do react homework</span>
         </li>
       </ul>
     </main>
